cc1954e103c8f337f92630f36207137f3ba14651
amarjeetsahoo <amarjeethappysahoo@gmail.com>
2024-04-19 02:37:08 +0530

    added DBContext,AppSettings

--- API/Program.cs
+++ API/Program.cs
@@ -1,6 +1,17 @@
+using API.Data;
+using API.Models;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(options =>
+options.UseSqlite(builder.Configuration.GetConnectionString("DefaultConnection")));
+builder.Services.AddIdentity<AppUser, IdentityRole>()
+    .AddEntityFrameworkStores<AppDbContext>()
+    .AddDefaultTokenProviders();
+
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
