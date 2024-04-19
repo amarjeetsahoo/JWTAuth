1019d0bfb205bf1c16574452cb91f6f1ba792919
amarjeetsahoo <amarjeethappysahoo@gmail.com>
2024-04-19 21:47:24 +0530

    added: RegisterUser

---
+++ API/DTOs/AuthResponseDto.cs
@@ -0,0 +1,9 @@
+﻿namespace API.DTOs
+{
+    public class AuthResponseDto
+    {
+        public string Token { get; set; } = String.Empty;
+        public bool IsSuccess { get; set; }
+        public string? Message { get; set; }
+    }
+}
---
+++ API/DTOs/RegisterDto.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace API.DTOs
+{
+    public class RegisterDto
+    {
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; } = String.Empty;
+
+        [Required]
+        public string FullName { get; set; } = String.Empty;
+
+        public string Password { get; set; } = String.Empty;
+
+        public List<string>? Roles { get; set; }
+    }
+}
